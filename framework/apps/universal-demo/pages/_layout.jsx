14f4f32029bfe58287ce688c0bc93ef54b3040bd
Rohitsharma6377 <netcurion@outlook.com>
2025-12-12 12:50:54 +0530

    push

---
+++ framework/apps/universal-demo/pages/_layout.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Layout({ children }) {
+  return <div className="min-h-screen bg-white">{children}</div>;
+}
---
+++ framework/apps/universal-demo/pages/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Home() {
+  return <div className="p-10 text-center"><h1 className="text-4xl font-bold">Welcome</h1></div>;
+}
